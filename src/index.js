bbfac065a7e7d29fe29727e83a0f79bd388205af
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-01-03 22:44:15 +0530

    Implement the display post feature by fetching post api through react router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,109 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter, Routes, Route, useParams, NavLink } from 'react-router-dom';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Home = () => {
+  const [posts, setPosts] = useState(null);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!response.ok) {
+        console.log("Error fetching posts: ");
+      }
+      const data = await response.json();
+      setPosts(data);
+    }
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        {posts ? posts.map((post, index) => (
+          <NavLink style={{ display: "block" }} to={`/post/${post.id}`} key={index}>{post.id } - {post.title}</NavLink>)) : "No Posts"}
+      </ul>
+    </div>
+  )
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  )
+};
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+    </div>
+  )
+};
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+    </div>
+  )
+};
+
+const SayUser = () => {
+  const params = useParams();
+  console.log("params: ", params);
+  return (
+    <div>
+      <h1>My name is {params.userName}</h1>
+    </div>
+  )
+};
+
+const PostPage = () => {
+  const params = useParams();
+  const postId = params.postId;
+
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((data) => data.json())
+      .then((data) => setPost(data));
+  }, [])
+
+  return (
+    <div>
+      <h1>post</h1>
+      {post ? <> <p>{post.title}</p> <p>{post.body}</p></> : "No post"}
+    </div>
+  )
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+
+        <Route path='/post/:postId' element={<PostPage />} />
+        <Route path='/user/:userName' element={<SayUser />} />
+
+        <Route path='account'>
+          <Route path='profile' element={<Profile />} />
+          <Route path='setting' element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
